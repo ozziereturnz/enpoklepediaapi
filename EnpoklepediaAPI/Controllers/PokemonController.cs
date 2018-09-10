a7bc5109f957bd5eb7873d68f97a73ffb8d16fa3
ozziereturnz <ozziereturnz@gmail.com>
2018-09-10 13:26:03 +1000

    API boilerplate

---
+++ EnpoklepediaAPI/Controllers/PokemonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EnpoklepediaAPI.Typings;
+
+namespace EnpoklepediaAPI.Controllers
+{
+    public class PokemonController : ApiController
+    {
+		// GET api/pokemon
+		[HttpGet]
+		public IEnumerable<Pokemon> Get()
+		{
+			return new Pokemon[] { new Pokemon() };
+		}
+
+		// GET api/pokemon/ID
+		[HttpGet]
+		public Pokemon Get(Guid ID)
+		{
+			return new Pokemon();
+		}
+    }
+}
---
+++ EnpoklepediaAPI/Typings/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnpoklepediaAPI.Typings
+{
+	/// <summary>
+	/// A record of a Pokemon
+	/// </summary>
+	public class Pokemon
+	{
+		public Guid ID { get; set; }
+		public short Number { get; set; }
+		public string Name { get; set; }
+		public string ImageURL { get; set; }
+	}
+}
